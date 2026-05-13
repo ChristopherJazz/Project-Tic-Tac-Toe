6c624a28939774b93e908e7e356636bc7e54ff00
Christopher Jazz Esguerra <christopher.esguerra2@gmail.com>
2026-05-14 01:28:53 +0800

    Cell clickable and shows whos turn

--- script.js
+++ script.js
@@ -1,32 +1,44 @@
 const cellBtn = document.querySelectorAll(".cell");
 const statusText = document.querySelector("#statusText");
 const restartBtn = document.querySelector("#restartBtn");
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
-
-cellBtn.forEach(cell => {
-
-    cell.addEventListener('click', function (event) {
-    cell.textContent = "X";
-    if (cell.textContent !==""){
-        return;
-    }
-})});
-
+let options = ["", "", "", "", "", "", "", "", ""];
 let currentPlayer = "X";
+let running = false;
+
+initializeGame();
+
+
+function initializeGame() {
+    statusText.textContent = `${currentPlayer}'s turn`;
+
+    cellBtn.forEach(cell => {
+        cell.addEventListener("click", () => {
+            if (cell.textContent !== "") {
+                return;
+            }
+            cell.textContent = currentPlayer;
+            changePlayer();
+        });
+    });
+}
+
 
 function changePlayer(){
     currentPlayer =
-    (currentPlayer == "X")
-    ? "O"
-    : "X";
+    (currentPlayer == "X") ? "O": "X";
 
-    statusText.textContent =
-    `${currentPlayer}'s turn`;
+    statusText.textContent = `${currentPlayer}'s turn`;
+    
 }
-
-let board = [
- "", "", "",
- "", "", "",
- "", "", ""
-];
 
